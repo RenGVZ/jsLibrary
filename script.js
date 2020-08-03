47bd27d50cd4150d35b7c75d78404558e3204734
Gavin Garza <48096041+RenGVZ@users.noreply.github.com>
2020-08-03 23:21:05 +0900

    reverted back to render function from before

--- script.js
+++ script.js
@@ -7,13 +7,7 @@ const bookRead = document.getElementById('read');
 
 const bookRow = document.querySelector('.row');
 
-let myLibrary = [
-  // [Book = {
-  //   title: 'Artemis Fowl',
-  //   author: 'Eoin Colfer',
-  //   pages: 547
-  // }]
-];
+let myLibrary = [];
 
 function Book(title, author, pages) {
   this.title = title,
@@ -59,31 +53,3 @@ function render() {
 }
 
 
-
-// function addBookToLibrary() {
-//   event.preventDefault();
-//   // create a new dom div with the class of: book col-sm-4
-//   let newBook = document.createElement('div');
-//   newBook.classList = 'book col-sm-4';
-//   // give it the title taken from the form
-//   let title = document.createElement('h1');
-//   title.innerText = bookTitle.value;
-//   title.classList = 'book-title'
-//   // give it the author taken from the form
-//   let author = document.createElement('p');
-//   author.innerText = bookAuthor.value;
-//   author.classList = 'book-author'
-//   // give it the pages taken from the form
-//   let pages = document.createElement('p');
-//   pages.innerText = bookPages.value;
-//   pages.classList = 'book-pages';
-//   // give it a read status taken from the form
-  
-//   // append these attributes to the newBook
-//   newBook.appendChild(title);
-//   newBook.appendChild(author);
-//   newBook.appendChild(pages);
-//   // append newBook to the end of BookRow
-//   bookRow.appendChild(newBook);
-//   // 
-// }
